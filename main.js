7679e6f4b9a70f78be006ec11fc4d3eea39177db
nenadvuk <nenadvukasinovic.rs@gmail.com>
2020-02-21 22:16:07 +0100

    something

--- main.js
+++ main.js
@@ -24,7 +24,7 @@ const drawnNumberBox = document.querySelector(".drawn-numbers-box");
 const choise = document.querySelector("#choise");
 const play = document.querySelector("#play");
 const randomNum = document.querySelector("#addRandomNumbers");
-
+const circle = document.querySelectorAll(".circle");
 
 let checkedFields = document.querySelectorAll("td");
 let chosenNumbers = [];
@@ -60,66 +60,59 @@ function timer(ms) {
 
 function blockAdding() {
 
-
 	randomNum.style.background = "linear-gradient(to bottom, #e8ebec 5%, #f70000 100%)";
 	randomNum.style.pointerEvents = "none";
 	for (let i = 0; i < 48; i++) {
-		document.querySelectorAll(".circle")[i].style.pointerEvents = "none";
+		circle[i].style.pointerEvents = "none";
 		checkedFields[i].style.pointerEvents = "none";
 	}
 	for (let j = 0; j < allColors.length; j++) {
 		allColors[j].style.pointerEvents = "none";
 	}
-
-
 }
 
 randomNum.style.background = "linear-gradient(to bottom, #e8ebec 5%, #02740b 100%)"
 randomNum.style.animation = `tada 1s ease-in`;
+
 // Printing numbers to ticket
 
 
 function ticket() {
+
 	for (let j = 0; j < sortedSlicedArray.length; j++) {
 		if (sortedSlicedArray[j] < 10) {
 			document.getElementById(`col${count}${j + 1}`).innerHTML = `0${sortedSlicedArray[j]}`;
 
 		} else document.getElementById(`col${count}${j + 1}`).innerHTML = sortedSlicedArray[j];
-
 	}
+}
+
+function playStyle() {
+	play.style.background = "linear-gradient(to bottom, #e8ebec 5%, #02740b 100%)";
 }
 
 // Reseting cirle colors
 
 function resetCircleColor() {
 	for (let i = 0; i < 48; i++) {
-		document.querySelectorAll(".circle")[i].style.border = "1px solid gray";
-
+		circle[i].style.border = "1px solid gray";
 	}
-
-
-
 }
 
 // Generating 6 random numbers
 
 randomNum.addEventListener("mousedown", function () {
 
-	play.style.animation = "tada 1s ease-in";
 	resetCircleColor();
-	play.style.background = "linear-gradient(to bottom, #e8ebec 5%, #02740b 100%)";
+	playStyle();
 	count++;
 	sortedSlicedArray = gen(6).sort((a, b) => a - b);
 	arrayAdd();
 	ticket();
-
-
 	if (count === 8) {
-		blockAdding()
-
+		blockAdding();
+		play.style.animation = "tada 1s ease-in";
 	}
-
-
 })
 
 // All colors
@@ -127,7 +120,6 @@ randomNum.addEventListener("mousedown", function () {
 for (let y = 0; y < allColors.length; y++) {
 
 	allColors[y].addEventListener("mousedown", function () {
-
 		if (!luckySixGame["chosenSix"] /* && !luckySixGame["chosenEight"] */) {
 			count++;
 			this.style.pointerEvents = "none";
@@ -135,25 +127,20 @@ for (let y = 0; y < allColors.length; y++) {
 			for (let i = 0; i < sortedSlicedArray.length; i++) {
 				document.querySelectorAll(`.circle-${COLORS[y]}`)[i].style.border = `1.5px solid ${COLORS[y]}`
 			}
-			setTimeout(function(){
-				play.style.background = "linear-gradient(to bottom, #e8ebec 5%, #02740b 100%)";
-				play.style.animation = "tada 1s ease-in"
-			},300)
+			setTimeout(function () {
+				playStyle()
+				resetCircleColor();
+
+			}, 300)
 
 			arrayAdd();
 			ticket();
-			setTimeout(function () {
-				resetCircleColor();
-				play.style.background = "linear-gradient(to bottom, #e8ebec 5%, #02740b 100%)";
-			}, 900)
 
 		}
 		if (count === 8) {
-			blockAdding()
-			
+			blockAdding();
+			play.style.animation = "tada 1s ease-in";
 		}
-
-
 	})
 }
 
@@ -161,18 +148,19 @@ for (let y = 0; y < allColors.length; y++) {
 // Numbers choise
 
 for (let i = 0; i < checkedFields.length; i++) {
-	
+
 	adder.style.pointerEvents = "none";
 	checkedFields[i].addEventListener("mousedown", function () {
+		for (let n = 0; n < allColors.length; n++) {
+			allColors[n].style.pointerEvents = "none";
+
+		}
 		randomNum.style.pointerEvents = "none";
 		randomNum.style.background = "linear-gradient(to bottom, #e8ebec 5%, #f70000 100%)";
 		if (count === 8) {
 			blockAdding();
-	
 		}
-		
 		if (!luckySixGame["chosenSix"]) {
-
 			checkedNumbers++;
 			this.style.pointerEvents = "none";
 			for (let j = 0; j < 6; j++) {
@@ -202,63 +190,51 @@ for (let i = 0; i < checkedFields.length; i++) {
 					circleColor.style.border = "1.5px solid black"
 				}
 			}
-
 			let clickedNumber = Number(this.id);
 			chosenNumbers.push(clickedNumber);
-			
 		}
-
 		if (checkedNumbers === 6) {
 			luckySixGame["chosenSix"] = true;
 			adder.style.pointerEvents = "all";
-			adder.style.background = "linear-gradient(to bottom, #e8ebec 5%, #02740b 100%)"
-			setTimeout(function(){
+			adder.style.background = "linear-gradient(to bottom, #e8ebec 5%, #02740b 100%)";
+			setTimeout(function () {
 				adder.style.animation = "tada 1s ease-in";
-			},500)
-
+			}, 500)
 		}
-
-
 	})
 }
 
 // Adding chosen numbers to a ticket
 
 function add() {
+	for (let n = 0; n < allColors.length; n++) {
+		allColors[n].style.pointerEvents = "all";
 
+	}
 	count++;
 	randomNum.style.pointerEvents = "all";
-	randomNum.style.background = "linear-gradient(to bottom, #e8ebec 5%, #02740b 100%)"
+	randomNum.style.background = "linear-gradient(to bottom, #e8ebec 5%, #02740b 100%)";
 	slicedArray = chosenNumbers.slice(index);
 	sortedSlicedArray = slicedArray.sort((a, b) => a - b);
-	setTimeout(function(){
-		play.style.background = "linear-gradient(to bottom, #e8ebec 5%, #02740b 100%)";
-		play.style.animation = "tada 1s ease-in"
-	},300)
+	playStyle();
 	arrayAdd();
 	resetCircleColor();
-
 	adder.style.pointerEvents = "none";
 	ticket();
 	if (luckySixGame["chosenSix"]) {
-
 		for (let i = 0; i < chosenNumbers.length; i++) {
 			checkedFields[chosenNumbers[i] - 1].style.pointerEvents = "all";
-
 		}
 		console.log(slicedArray)
 		luckySixGame["chosenSix"] = false;
 		checkedNumbers = 0;
 		index += 6;
-
 	}
 	if (count === 8) {
 		blockAdding();
-
+		play.style.animation = "tada 1s ease-in";
 	}
 	adder.style.background = "linear-gradient(to bottom, #e8ebec 5%, #f70000 100%)";
-
-
 
 }
 
@@ -287,38 +263,28 @@ function gen(val = 35) {
 
 console.log(num)
 
-
-
 // Displaying 35 numbers with delay
 async function playGame() {
-	
+
 	document.querySelector(".tg").style.animation = "bounceOut 1s ease-in";
 	setTimeout(function () {
 		drawnNumberBox.style.display = "block";
 	}, 900)
-
 	drawnNumberBox.style.animation = "scale 1s ease-in"
 	setTimeout(function () {
 		document.querySelector(".tg").style.display = "none"
 	}, 900)
 	drumBall.style.display = "block";
-
 	for (let j = 0; j < num.length; j++) {
 		for (let n = 0; n < num.length; n++) {
 			for (let b = 0; b <= 6; b++) {
 				for (let a = 1; a <= count; a++) {
 					if (num[j] === luckySixGame["arrays"][`arr${a}`][0][b]) {
-
 						setTimeout(function () {
 							document.querySelector("#col" + a + (b + 1)).style.fontWeight = "1000";
-
 						}, 3000);
-
 					}
 				}
-
-
-
 			}
 			document.getElementById("res").innerHTML = `${num[j]}`;
 			/* document.getElementById("res").classList.add("ball-number-anim") */
@@ -351,15 +317,9 @@ async function playGame() {
 
 			} if (num[j] === luckySixGame["colors"]["black"][n]) {
 				drumBall.style.background = "radial-gradient(circle at 30px 30px, #606060, #000)"
-
 			}
-
 		} await timer(3000)
-
-
 	}
-
-
 }
 
 
